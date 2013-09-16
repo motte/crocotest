fb533fd340eda89e1ae220beb50eef2ca08b1572
motte <michaelotte1@gmail.com>
2013-09-16 11:21:51 -0400

    incomple views for local crocodoc app

--- crocotest/crocodoc/models.py
+++ crocotest/crocodoc/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
-from djcroco.fileds import CrocoField
+from djcroco.fields import CrocoField
 
 class Example(models.Model):
     name = models.CharField(max_length=255)
     document = CrocoField(thumbnail_field='test_thumbnail')
-    test_thumbnail = models.ImageField(upload_to='whatever/')
+    test_thumbnail = models.ImageField(upload_to='crocodoc/uploads/')
     
 
     def __unicode__(self):
--- crocotest/crocodoc/views.py
+++ crocotest/crocodoc/views.py
@@ -1,1 +1,46 @@
-# Create your views here.
+
+
+# local system - specific
+import crocodoc
+from crocodoc import CrocodocErorr
+
+
+from django.http import Http404, HttpResponse, HttpResponseRedirect
+from django.views.generic import View
+
+class CrocoDocView(View):
+    redirect = None
+
+    def get(self, request, *args, **kwargs):
+        uuid = kwargs.pop('uuid', None)
+        if uuid is None:
+            raise Http404
+
+        try:
+            params = {}
+            qs_params = request.GET
+            
+            bool_params = ("editable", "admin", "downloadable", "copyprotected", "demo")
+            for p in bool_params:
+                if p in qs_params:
+                    params[p] = True
+            
+            if 'user_id' in qs_params and 'user_name' in qs_params:
+                params['user'] = {
+                    'id': qs_params['user_id'],
+                    'name': qs_params['user_name'],
+                }
+            
+            if 'filter' in qs_params:
+                params['filter'] = qs_params['filter']
+            
+            if 'sidebar' in qs_params:
+                params['sidebar'] = qs_params['sidebar']
+
+            session = crocodoc.session.create(uuid, **params)
+        except crocodoc.CrocodocError as e:
+            return HttpResponse(content=e.response_content,
+                                status=e.status_code)
+        
+        url = 'https://crocodoc.com/view/{0}'.format(session)
+    
