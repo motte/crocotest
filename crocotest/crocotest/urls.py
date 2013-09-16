6b4c5c949e0909f2c2a8a276014f49c5869c3645
motte <michaelotte1@gmail.com>
2013-09-16 10:31:28 -0400

    base commit + crocodoc

---
+++ crocotest/crocotest/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'crocotest.views.home', name='home'),
+    # url(r'^crocotest/', include('crocotest.foo.urls')),
+
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+)
