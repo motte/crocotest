d749ef307ae49a3d472b5911296093124e9ec871
motte <michaelotte1@gmail.com>
2013-09-23 11:32:51 -0400

    urls setup for index with a simple view

--- crocotest/crocotest/urls.py
+++ crocotest/crocotest/urls.py
@@ -1,3 +1,4 @@
+
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
@@ -6,30 +7,12 @@ admin.autodiscover()
 from crocod import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
-
-    url(r'^croco_document/(?P<uuid>[-\w]+)$',
-        views.CrocoDocumentView.as_view(redirect=True),
-        name='croco_document_url'),
-    url(r'^croco_document_content/(?P<uuid>[-\w]+)$',
-        views.CrocoDocumentView.as_view(),
-        name='croco_document_content_url'),
-    url(r'^croco_document_download/(?P<uuid>[-\w]+)$',
-        views.CrocoDocumentDownload.as_view(),
-        name='croco_document_download'),
-    url(r'^croco_thumbnail_download/(?P<uuid>[-\w]+)$',
-        views.CrocoThumbnailDownload.as_view(),
-        name='croco_thumbnail_download'),
-    url(r'^croco_text_download/(?P<uuid>[-\w]+)$',
-        views.CrocoTextDownload.as_view(),
-        name='croco_text_download'),
+    url(r'', include('djcroco.urls')),
 
 
-    # Examples:
-   # url(r'^$', 'crocotest.views.home', name='home'),
- #   url(r'^crocotest/', include('crocotest.crocod.urls')),
+    url(r'^$', 'crocod.views.index', name='home'),
+   # url(r'^crocotest/', include('crocotest.crocod.urls')),
 
-    # the djcroco url
     url(r'', include('djcroco.urls')),
 
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
